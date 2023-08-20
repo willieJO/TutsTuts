57cb4df57c22664078b3829947504fbc8ea83253
WillieJO <willie.oliveira@grxsolucoes.com.br>
2023-08-19 21:33:24 -0300

    Inicio do repositorio

---
+++ BackEnd/src/main/java/br/com/edu/tutstuts/TutsTutsApplication.java
@@ -0,0 +1,13 @@
+package br.com.edu.tutstuts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutsTutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutsTutsApplication.class, args);
+	}
+
+}

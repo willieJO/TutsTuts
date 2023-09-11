f8d213e28bc303eb423299fc179f6b4237d4d418
a <a>
2023-09-11 09:26:32 -0300

    Edição de usuarios tanto empresa quanto os comum

--- BackEnd/src/main/java/br/com/edu/tutstuts/controller/UsuarioController.java
+++ BackEnd/src/main/java/br/com/edu/tutstuts/controller/UsuarioController.java
@@ -1,25 +1,45 @@
 package br.com.edu.tutstuts.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.edu.tutstuts.model.UsuarioComum;
 import br.com.edu.tutstuts.model.UsuarioEmpresa;
 import br.com.edu.tutstuts.model.Usuario;
-import br.com.edu.tutstuts.repository.UsuarioComumRepository;
-import br.com.edu.tutstuts.repository.UsuarioEmpresaRepository;
 import br.com.edu.tutstuts.repository.UsuarioRepository;
 import br.com.edu.tutstuts.service.UsuarioService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/Usuario")
 public class UsuarioController {
 	@Autowired
 	private UsuarioService _service;
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@GetMapping
+	public List<Usuario> list(){
+		return usuarioRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> findById(@PathVariable Long id){
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
+		if(usuario.isPresent()) {
+			return ResponseEntity.ok(usuario.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
 	
 	@PostMapping("AdicionarUsuarioEmpresa")
 	private void AdicionarUsuarioEmpresa(@RequestBody UsuarioEmpresa usuario) {
@@ -29,5 +49,20 @@ public class UsuarioController {
 	private void AdicionarUsuario(@RequestBody UsuarioComum usuario) {
 		_service.AdicionarUsuario(usuario);
 	}
+	
+	@PutMapping("AtualizarEmpresa/{id}")
+	public ResponseEntity<UsuarioEmpresa> updateEmpresa(@PathVariable Long id,
+			@Valid @RequestBody UsuarioEmpresa usuario){
+		UsuarioEmpresa userSaved = _service.updateEmpresa(id, usuario);
+		return ResponseEntity.ok(userSaved);
+	}
+	
+	@PutMapping("UsuarioComum/{id}")
+	public ResponseEntity<UsuarioComum> update(@PathVariable Long id,
+			@Valid @RequestBody UsuarioComum usuario){
+		UsuarioComum userSaved = _service.update(id, usuario);
+		return ResponseEntity.ok(userSaved);
+	}
+	
 	
 }
--- BackEnd/src/main/java/br/com/edu/tutstuts/service/UsuarioService.java
+++ BackEnd/src/main/java/br/com/edu/tutstuts/service/UsuarioService.java
@@ -1,19 +1,18 @@
 package br.com.edu.tutstuts.service;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import br.com.edu.tutstuts.model.UsuarioComum;
 import br.com.edu.tutstuts.model.UsuarioEmpresa;
 import br.com.edu.tutstuts.repository.UsuarioComumRepository;
 import br.com.edu.tutstuts.repository.UsuarioEmpresaRepository;
-import br.com.edu.tutstuts.repository.UsuarioRepository;
 
 @Service
 public class UsuarioService {
 	@Autowired
 	private UsuarioEmpresaRepository _empresaRepository;
-	@Autowired
-	private UsuarioRepository _usuarioRepository;
 	@Autowired
 	private UsuarioComumRepository _usuarioComumRepository;
 	
@@ -25,4 +24,31 @@ public class UsuarioService {
 		_usuarioComumRepository.save(usuario);
 	}
 	
+	public UsuarioEmpresa updateEmpresa(Long id, UsuarioEmpresa usuario) {
+		UsuarioEmpresa usuarioEditado = findUserByIdEmpresa(id);
+		BeanUtils.copyProperties(usuario, usuarioEditado, "id");
+		return _empresaRepository.save(usuarioEditado);
+	}
+	
+	public UsuarioEmpresa findUserByIdEmpresa(Long id) {
+		UsuarioEmpresa userSaved = _empresaRepository.findById(id)
+				.orElseThrow(
+				(() -> new EmptyResultDataAccessException(1)));
+		return userSaved;
+	}
+	
+	public UsuarioComum update(Long id, UsuarioComum usuario) {
+		UsuarioComum usuarioEditado = findUserById(id);
+		BeanUtils.copyProperties(usuario, usuarioEditado, "id");
+		return _usuarioComumRepository.save(usuarioEditado);
+	}
+	
+	public UsuarioComum findUserById(Long id) {
+		UsuarioComum userSaved = _usuarioComumRepository.findById(id)
+				.orElseThrow(
+				(() -> new EmptyResultDataAccessException(1)));
+		return userSaved;
+	}
+	
+
 }

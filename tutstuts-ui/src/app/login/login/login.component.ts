3472e0e6f69e9bc523ad7e675b1d08f2f87679e5
a <a>
2023-09-25 11:22:26 -0300

    criacao da tela de login

--- tutstuts-ui/src/app/login/login/login.component.ts
+++ tutstuts-ui/src/app/login/login/login.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { MessageService } from 'primeng/api';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,29 +7,50 @@ import { MessageService } from 'primeng/api';
   styleUrls: ['./login.component.css']
 })
 export class LoginUiComponent {
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService) {
+
+   }
+
   showPassword = false;
-
-
   togglePasswordVisibility(passwordInput: HTMLInputElement) {
     this.showPassword = !this.showPassword;
     passwordInput.type = this.showPassword ? 'text' : 'password';
   }
-  
+
   title: string = 'Login';
-  login = {   
-     email: '' ,
-     senha: ''
-  }; 
+  login = {
+     email: null,
+     senha: null
+  };
+
+  esqueceuSenha() {
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Esqueceu a senha',
+      detail: 'Em breve',
+      life: 2000
+    });
+  }
 
   realizarLogin() {
-    this.messageService.add({
-      severity: 'success',
-      summary: 'Login bem-sucedido',
-      detail: 'Você foi autenticado com sucesso.',
-      life: 2000 
-    });
-    
+    const numeroAleatorio = Math.floor(Math.random() * 10) + 1;
+    if (numeroAleatorio % 2 == 0) {
+      this.messageService.add({
+        severity: 'success',
+        summary: 'Login bem-sucedido',
+        detail: 'Você foi autenticado com sucesso.',
+        life: 2000
+      });
+    } else {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Erro',
+        detail: 'Credenciais invalidas',
+        life: 2000
+      });
+    }
+
+
   }
-  
+
 }

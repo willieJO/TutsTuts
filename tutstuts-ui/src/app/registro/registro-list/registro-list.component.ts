1895dea0bc949b3d57d6257547f4969cd64dd9de
fernandaagustoni <fernanda_agustoni@hotmail.com>
2023-10-08 19:37:15 -0300

    Tela Registro Usuario

---
+++ tutstuts-ui/src/app/registro/registro-list/registro-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-list',
+  templateUrl: './registro-list.component.html',
+  styleUrls: ['./registro-list.component.css']
+})
+
+export class RegistroListComponent {
+  title = 'Registro Usuário';
+  value = '';
+}
---
+++ tutstuts-ui/src/app/registro/registro.module.ts
@@ -0,0 +1,24 @@
+import { NgModule, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { TooltipModule } from 'primeng/tooltip';;
+import { RegistroListComponent } from './registro-list/registro-list.component';
+import { FormsModule } from '@angular/forms';
+import { PasswordModule } from "primeng/password";
+
+@NgModule({
+  declarations: [
+    RegistroListComponent
+  ],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    FormsModule,
+    TooltipModule,
+    PasswordModule
+  ],
+  exports: [
+    RegistroListComponent
+  ]
+})
+export class RegistroModule { }

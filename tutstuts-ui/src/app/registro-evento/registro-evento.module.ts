21f2f156890714bf4afca2e9cc6dfe2412a6c07c
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2023-10-16 16:38:24 -0300

    chore!: iniciando com as requisições

--- tutstuts-ui/src/app/registro-evento/registro-evento.module.ts
+++ tutstuts-ui/src/app/registro-evento/registro-evento.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegistroEventoListComponent } from './registro-evento-list/registro-evento-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [RegistroEventoListComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [RegistroEventoListComponent],
 })
 export class RegistroEventoModule {}

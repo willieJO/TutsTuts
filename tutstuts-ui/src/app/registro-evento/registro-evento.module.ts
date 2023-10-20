468cf0afa385039f02ba72356691658664fd1f05
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2023-10-20 09:22:35 -0300

    feat: fix import and exports registro.module

--- tutstuts-ui/src/app/registro-evento/registro-evento.module.ts
+++ tutstuts-ui/src/app/registro-evento/registro-evento.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RegistroEventoListComponent } from './registro-evento-list/registro-evento-list.component';
+import { ButtonModule } from 'primeng/button';
 import { FormsModule } from '@angular/forms';
+import { TooltipModule } from 'primeng/tooltip';
+
+import { RegistroEventoListComponent } from './registro-evento-list/registro-evento-list.component';
 
 @NgModule({
   declarations: [RegistroEventoListComponent],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ButtonModule, TooltipModule],
   exports: [RegistroEventoListComponent],
 })
 export class RegistroEventoModule {}

a7460f995bf8de312277ef1c3f49b96004dbdd24
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2023-10-20 09:27:37 -0300

    feat: add app routing an fix html app

--- tutstuts-ui/src/app/app-routing.module.ts
+++ tutstuts-ui/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegistroEventoListComponent } from './registro-evento/registro-evento-list/registro-evento-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: RegistroEventoListComponent },
+  { path: 'registroEvento', component: RegistroEventoListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

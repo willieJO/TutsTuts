8edd07f3e61563aac9becaa3467ca06cf96f4514
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2023-10-24 10:06:40 -0300

    feat: implementando messageService feedback

--- tutstuts-ui/src/app/registro-evento/registro-evento-list/registro-evento-list.component.ts
+++ tutstuts-ui/src/app/registro-evento/registro-evento-list/registro-evento-list.component.ts
@@ -1,19 +1,73 @@
 import { Component } from '@angular/core';
 import { RegistroEventoServiceService } from '../../Registro-Evento-Service/registro-evento-service.service';
 import { Evento } from '../../core/model';
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-registro-evento-list',
   templateUrl: './registro-evento-list.component.html',
+  providers: [MessageService],
   styleUrls: ['./registro-evento-list.component.css'],
 })
 export class RegistroEventoListComponent {
   title = 'Registro Evento';
   value = '';
-  constructor(public eventoserviceService: RegistroEventoServiceService) {}
+  constructor(
+    public eventoserviceService: RegistroEventoServiceService,
+    private messageService: MessageService
+  ) {}
   evento = new Evento();
 
   enviar() {
+    if (this.evento.nome == '' || this.evento.nome == null) {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Erro',
+        detail: 'O nome é obrigatorio',
+        life: 2000,
+      });
+      return;
+    }
+
+    if (this.evento.descricao == '' || this.evento.descricao == null) {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Erro',
+        detail: 'A descrição é obrigatoria',
+        life: 2000,
+      });
+      return;
+    }
+
+    if (this.evento.data_evento == '' || this.evento.data_evento == null) {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Erro',
+        detail: 'A Data do Evento é obrigatoria',
+        life: 2000,
+      });
+      return;
+    }
+
+    if (this.evento.categoria == '' || this.evento.categoria == null) {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Erro',
+        detail: 'A Categoria é obrigatoria',
+        life: 2000,
+      });
+      return;
+    }
+
+    if (this.evento.localidade == '' || this.evento.localidade == null) {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Erro',
+        detail: 'A Localidade é obrigatoria',
+        life: 2000,
+      });
+    }
+
     this.eventoserviceService.enviar(this.evento);
   }
 }
--- tutstuts-ui/src/app/registro-evento/registro-evento.module.ts
+++ tutstuts-ui/src/app/registro-evento/registro-evento.module.ts
@@ -3,12 +3,19 @@ import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { FormsModule } from '@angular/forms';
 import { TooltipModule } from 'primeng/tooltip';
+import { ToastModule } from 'primeng/toast';
 
 import { RegistroEventoListComponent } from './registro-evento-list/registro-evento-list.component';
 
 @NgModule({
   declarations: [RegistroEventoListComponent],
-  imports: [CommonModule, FormsModule, ButtonModule, TooltipModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ButtonModule,
+    TooltipModule,
+    ToastModule,
+  ],
   exports: [RegistroEventoListComponent],
 })
 export class RegistroEventoModule {}

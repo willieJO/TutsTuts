be74979540f9d46833b7d6982983d9485004ae81
WillieJO <willie.oliveira@grxsolucoes.com.br>
2023-10-26 22:41:50 -0300

    [fix]

--- tutstuts-ui/src/app/app.module.ts
+++ tutstuts-ui/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';

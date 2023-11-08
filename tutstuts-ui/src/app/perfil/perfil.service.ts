74d2b8f9253a8597e45b92a8299dffbdea4d3dc6
EnzoMbz <89809584+EnzowMb@users.noreply.github.com>
2023-11-08 11:30:14 -0300

    feat: implementendo metodos para o perfil service

--- tutstuts-ui/src/app/perfil/perfil.service.ts
+++ tutstuts-ui/src/app/perfil/perfil.service.ts
@@ -5,26 +5,54 @@ import { Observable } from 'rxjs';
 import { AuthService } from '../security/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PerfilService {
   baseUrl = 'http://localhost:8080'; // URL base da sua API
 
-  constructor(private http: HttpClient, private auth: AuthService) { }
-  
+  constructor(private http: HttpClient, private auth: AuthService) {}
+
   obterUsuarioPorId(): Observable<Usuario> {
     const url = `${this.baseUrl}/Usuario/${this.auth.getUserIdFromToken()}`;
     return this.http.get<Usuario>(url);
   }
-  atualizarUsuario(usuario:Usuario) {
+  atualizarUsuario(usuario: Usuario) {
     const httpOptions = {
       headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      })
+        'Content-Type': 'application/json',
+      }),
     };
-    if(localStorage.getItem("cnpj") != null) {
-      return this.http.put(this.baseUrl + `/Usuario/AtualizarEmpresa/${usuario.id}`, usuario, httpOptions).toPromise();
+    if (localStorage.getItem('cnpj') != null) {
+      return this.http
+        .put(
+          this.baseUrl + `/Usuario/AtualizarEmpresa/${usuario.id}`,
+          usuario,
+          httpOptions
+        )
+        .toPromise();
     }
-    return this.http.put(this.baseUrl + `/Usuario/UsuarioComum/${usuario.id}`, usuario, httpOptions).toPromise();
-  } 
+    return this.http
+      .put(
+        this.baseUrl + `/Usuario/UsuarioComum/${usuario.id}`,
+        usuario,
+        httpOptions
+      )
+      .toPromise();
+  }
+  obterEventos(): Promise<any> {
+    return this.http
+      .get(
+        this.baseUrl + '/Evento/ObterEventos' + this.auth.getUserIdFromToken()
+      )
+      .toPromise();
+  }
+  obterEventosCurtidoPeloUsuario(): Promise<any> {
+    return this.http
+      .get(
+        this.baseUrl +
+          '/Curtida/ObterEventosCurtidoPeloUsuario' +
+          this.auth.getUserIdFromToken()
+      )
+      .toPromise();
+  }
 }
